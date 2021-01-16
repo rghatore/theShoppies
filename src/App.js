db889c0a869bd14fc13bef99a5d108c1c7a56678
Rajdeep Ghatore <r.ghatore@gmail.com>
2021-01-15 18:46:40 -0700

    restructuring components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
-// import logo from './logo.svg';
+import { useState } from 'react';
 import './App.scss';
-import Header from './components/Header';
-import Main from './components/Main';
+import NominationList from './components/NominationList';
+import SearchBar from './components/SearchBar.js'
+import SearchResults from './components/SearchResults';
 
 function App() {
+
+  const [nominations, setNominations] = useState(["", "", "", "", ""])
+  const [results, setResults] = useState(["Superman", "Superman 2", "Superman 3"]);
+
   return (
     <div className="App">
       <>
-        <Header/>
-        <Main/>
+        <header>
+          <img id="logo" src="logo192.png" alt="logo" width={50}/>
+          <h1>The Shoppies</h1>
+        </header>
+        <main className="content_main">
+          <section className="content_search">
+            <SearchBar />
+            <p>Select your favourite movies to nominate for the Shoppies:</p>
+            <SearchResults results={results}/>
+          </section>
+          <aside className="content_nominations">
+            <h2 className="text_nominations">Nominations</h2>
+            <NominationList selected={nominations}/>
+          </aside>
+        </main>
       </>
     </div>
   );
